ccad58d0b89c62805d8896138d35185f121d87ff
azhar-g-bhat <Sahzar@users.noreply.github.com>
2024-01-01 15:21:08 -0500

    rr

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import checker from 'vite-plugin-checker';
 // ----------------------------------------------------------------------
 
 export default defineConfig({
-  base: "",
+  base: "/Azhar-gB-PersonalWebsite/",
   plugins: [
     react(),
     checker({
