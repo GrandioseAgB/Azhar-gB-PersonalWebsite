199c410fb6fae39e3a06335297cbfe288aa17046
azhar-g-bhat <Sahzar@users.noreply.github.com>
2024-01-01 15:49:41 -0500

    aa

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -3,5 +3,6 @@
 export const account = {
   displayName: 'Azhar gB',
   email: 'azhar.gulzar@gmail.com',
-  photoURL: '/assets/images/avatars/head-bearded.jpg',
+  // photoURL: '/assets/images/avatars/head-bearded.jpg',
+  photoURL: '/head-bearded.jpg',
 };
